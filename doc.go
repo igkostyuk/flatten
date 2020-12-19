cfbf4ede60a64dc67866a21763f8f71abe15837c
Igor kostyuk <ig.fire@gmail.com>
2020-12-19 21:07:52 +0200

    docs: add doc.go file

---
+++ doc.go
@@ -0,0 +1,11 @@
+// Package flatten conver slice matrix to slice.
+//
+// Usage
+//  a := [][]int{
+//      {1, 2},
+//		{4, 3},
+//	}
+//	b, _ := flatten.Clockwise(a)
+//	fmt.Printf("%v is %v after flatten", b, a)
+//
+package flatten
